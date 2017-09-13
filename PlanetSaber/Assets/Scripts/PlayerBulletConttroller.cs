368f0496b8cdc2de01afd9e61ee9a6853ed444b3
minimo <minimo.mobile@gmail.com>
2017-09-14 06:39:04 +0900

    Update.

---
+++ PlanetSaber/Assets/Scripts/PlayerBulletConttroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletConttroller : MonoBehaviour {
+	//弾速
+	float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		Quaternion rot = Camera.main.transform.rotation;
+		this.transform.rotation = rot;
+		Destroy(this.gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0, 0, this.speed);
+	}
+}
